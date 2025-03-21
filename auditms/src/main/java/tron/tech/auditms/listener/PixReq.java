a04405860b47f03a0645cf1e95e011fb8ecec1ef
Alexandre Alves <alexandrealveees@outlook.com>
2025-03-21 11:34:57 -0300

    update pfms files

--- auditms/src/main/java/tron/tech/auditms/listener/PixReq.java
+++
@@ -1,6 +0,0 @@
-package tron.tech.auditms.listener;
-
-import java.math.BigDecimal;
-
-public record PixReq(String channel, String from, String to, BigDecimal value) {
-}
---
+++ pfms/src/main/java/tron/tech/pfms/listener/PixReq.java
@@ -0,0 +1,6 @@
+package tron.tech.pfms.listener;
+
+import java.math.BigDecimal;
+
+public record PixReq(String channel, String from, String to, BigDecimal value) {
+}
